cc851f6db9e81d18dae4c04923a5674dbb990ea3
Bastien NEGRO <bastien.negro@viacesi.fr>
2021-04-16 04:00:39 +0200

    ST-0-01 fait

---
+++ MGL 846/src/main/java/Employee/testableConn.java
@@ -0,0 +1,38 @@
+package main.java.Employee;
+
+/*
+CLASS Creation Steps:
+1.conn
+2.welcome_page
+3.login_page
+4.details_page
+5.add_employee
+6.view_employee
+7.print_data
+8.remove_employee
+9.search_employee
+10.update_employee
+
+*/
+
+import java.sql.*;
+
+public class testableConn {
+
+	//Create two interface
+	public Connection c;  // used to set up connection with mysql
+	public Statement st;  // used to execute all queries of mysql
+	
+	public testableConn(String url, String user, String psswrd) {
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver"); // Load mysql jdbc driver
+			c = DriverManager.getConnection(url,user,psswrd);
+			st = c.createStatement(); // helpful to execute query
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+}
